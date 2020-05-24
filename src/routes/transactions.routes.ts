e91027ed663c59a28a4f6e3e29152a8822f16011
Lucas de Lucca <llucca54@gmail.com>
2020-05-24 15:10:57 -0300

    trasactions, categories, balance

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,84 @@
+import csv from 'csvtojson';
 import { Router } from 'express';
+import multer from 'multer';
+import toStream from 'tostream';
+import { getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import multerConfig from '../config/upload';
+import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  await transactionsRepository.delete(id);
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+const upload = multer(multerConfig);
+
+interface TransactionCSVItem {
+  title: string;
+  type: 'income' | 'outcome';
+  value: string;
+  category: string;
+}
+
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const readCSVStream = toStream(request.file.buffer);
+
+    const transactions: Transaction[] = [];
+
+    await csv()
+      .fromStream(readCSVStream)
+      .subscribe(
+        async ({ title, type, value, category }: TransactionCSVItem) => {
+          const createTransaction = new CreateTransactionService();
+          const transaction = await createTransaction.execute({
+            title,
+            value: parseInt(value, 10),
+            type,
+            category,
+          });
+
+          transactions.push(transaction);
+        },
+      );
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
